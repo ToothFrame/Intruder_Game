e283637a7a394c65043b5e9bb67f4111161140e5
Luigi <gigi.demarco@hotmail.it>
2023-06-27 22:43:53 +0100

    movement + lightswitch
    Added player movement and sprint
    also fixed light switch bug

--- Intruder/Assets/Scripts/PlayerController.cs
+++ Intruder/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterController controller;
+    public float playerSpeed = 10f;
+    public float sprintMultiplier = 1.5f;
     // Start is called before the first frame update
+
     void Start()
     {
         
@@ -13,6 +17,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if(Input.GetKey("left shift"))
+        {
+            Sprint(controller, move);
+        }
+        else
+        {
+            Movement(controller, move);
+        }
     }
+
+    void Sprint(CharacterController controller, Vector3 move)
+    {
+        controller.Move(move * Time.deltaTime * sprintMultiplier * playerSpeed);
+    }
+    void Movement(CharacterController controller, Vector3 move)
+    {
+        controller.Move(move * Time.deltaTime * playerSpeed);
+    }
+
 }
