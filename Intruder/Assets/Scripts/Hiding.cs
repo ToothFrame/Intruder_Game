387a0229fc33590a2aec941447c11038a23e4a67
Luigi <gigi.demarco@hotmail.it>
2023-07-13 19:26:01 +0100

    Aggiunto crouch
    si deve pulire il codice pero'.
    Non so come ma devo fare in modo di spostare il player sotto all'hiding place automaticamente

---
+++ Intruder/Assets/Scripts/Hiding.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hiding : MonoBehaviour, IInteractable
+{
+    public GameObject player;
+    
+
+    public void Interact()
+    {
+        
+
+    }
+}
--- Intruder/Assets/Scripts/PlayerController.cs
+++ Intruder/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
     public float playerSpeed = 10f;
     public float sprintMultiplier = 1.5f;
-    // Start is called before the first frame update
+    public Vector3 scale = new Vector3(1f, 1f, 1f);
+    public bool isHiding = false;
 
     void Start()
     {
@@ -15,7 +16,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -25,6 +26,7 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey("left shift"))
         {
             Sprint(controller, move);
+            
         }
         else
         {
@@ -32,13 +34,32 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown("c")) { isHiding = Hide(isHiding); } 
+    }
+
     void Sprint(CharacterController controller, Vector3 move)
     {
         controller.Move(move * Time.deltaTime * sprintMultiplier * playerSpeed);
     }
+
     void Movement(CharacterController controller, Vector3 move)
     {
         controller.Move(move * Time.deltaTime * playerSpeed);
+    }
+
+    public bool Hide(bool isHiding)
+    {
+        if (!isHiding)
+        {
+            transform.localScale = new Vector3(1f, 0.5f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        return !isHiding;
     }
 
 }
